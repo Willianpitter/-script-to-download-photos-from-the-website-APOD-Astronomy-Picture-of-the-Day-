c243e6aef5c28d78ecebb2690aff8f63024ab619
Willian Pitter <willianpiterh@gmail.com>
2018-11-21 12:36:10 -0200

    Update apod2.php
    Último comite para o programa de download de imagens do site APOD

--- apod2.php
+++ apod2.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Created by PhpStorm.
+ * Created by.
  * User: research
  * Date: 2018-11-14
  * Time: 12:48
  */
 
-/* ue
+/*
  * há duas formas de um qq software se associar a outro
  * via código fonte
  * via código binário (exemplo via DLLs)
@@ -17,10 +17,9 @@
  * req: 1) que dia é hoje
  */
 
-define("APOD_BASE_URL", "https://apod.nasa.gov/apod/");
+define ("APOD_BASE_URL", "https://apod.nasa.gov/apod/");
 
-function hojeY2M2D2()
-{
+function hojeY2M2D2(){
     return date("ymd"); //"181114" quando for 2018-11-14
 }//hojeY2M2D2
 
@@ -40,17 +39,18 @@ function apodAbsoluteUrl(
      * TODO: como verificar a validade dos args
      * e não aceitá-los cegamente?!
      */
-    $pY = ($pY === false) ? intval(date("y")) : $pY;
-    $pM = ($pM === false) ? intval(date("m")) : $pM;
-    $pD = ($pD === false) ? intval(date("d")) : $pD;
+    $pY = ($pY===false) ? intval(date("y")) : $pY;
+    $pM = ($pM===false) ? intval(date("m")) : $pM;
+    $pD = ($pD===false) ? intval(date("d")) : $pD;
 
-    if ($pY === ($pM === ($pD === false))) {
+    if ($pY===($pM===($pD===false))){
         $ret = sprintf(
             "%sap%s.html",
             APOD_BASE_URL,
             hojeY2M2D2()
         );
-    } else {
+    }
+    else{
         //TODO: garantir o ano com 4d, o mês com 2d, e o dia com 2d
         $ret = sprintf(
             "%sap%s%s%s.html",
@@ -64,55 +64,39 @@ function apodAbsoluteUrl(
     return $ret;
 }//apodAbsoluteUrl
 
-function todosEnderecosDoMes($pAno, $pMes)
-{
+function todosEnderecosDoMes($pAno, $pMes){
     $ret = [];
-    for ($dia = 1; $dia <= numDiasNoMes($pAno, $pMes); $dia++) {
+    for ($dia=1; $dia<=numDiasNoMes($pAno, $pMes); $dia++){
         $url = apodAbsoluteUrl($pAno, $pMes, $dia);
         $ret[] = $url;
     }
     return $ret;
 }//todosEnderecosDoMes
 
-function numDiasNoMes($pAno, $pMes)
-{
-    $iAno = intval($pAno);
-    $iMes = intval($pMes);
-    switch ($iMes) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            return 31;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-        case 2:
-            return bissexto($iAno) ? 29 : 28;
+function numDiasNoMes ($pAno, $pMes){
+    $iAno = intval($pAno); $iMes = intval($pMes);
+    switch($iMes){
+        case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+        return 31;
+        case 4:case 6:case 9:case 11: return 30;
+        case 2: return bissexto($iAno) ? 29 : 28;
     }//switch
 }//numDiasNoMes
 
-function bissexto($pAno)
-{
+function bissexto($pAno){
     $iAno = intval($pAno);
-    $bC1 = $iAno % 4 === 0 && !$iAno % 100 === 0;
-    $bC2 = $iAno % 400 === 0;
+    $bC1 = $iAno%4===0 && !$iAno%100===0;
+    $bC2 = $iAno%400===0;
     return $bC1 || $bC2;
 }//bissexto
 
 /*
  * TODO: conferir muita robustez
  */
-function garantir2Digitos($pNum)
-{
+function garantir2Digitos ($pNum){
     $iNum = intval($pNum);
-    if ($iNum < 10)
-        return "0" . $pNum;
+    if ($iNum<10)
+        return "0".$pNum;
     else
         return $pNum;
 }//garantir2Digitos
@@ -123,15 +107,13 @@ function garantir2Digitos($pNum)
  * porque, mais tarde, esse HTML deverá ser procurado
  * para nele tentarmos encontrar um elemento img
  */
-function apodFetchSrcHtml()
-{
+function apodFetchSrcHtml(){
     $url = apodAbsoluteUrl();
     $html = getBinaryAtUrl($url);
 }//apodFetchSrcHtml
 
 //http://socrates.io/#VEursnf
-function getBinaryAtUrl($pUrl)
-{
+function getBinaryAtUrl($pUrl){
     $curlHandler = curl_init();
     $curlHandler = curl_init($pUrl);
     $bResult = curl_setopt
@@ -154,69 +136,138 @@ function getBinaryAtUrl($pUrl)
     return $bin;
 }//getBinaryAtUrl
 
+//http://socrates.io/#1H3PWww
+function urlDiretoParaImagemPorAnaliseDoHtml(
+    $pHtmlParaSerAnalisado,
+    $pStrMarcaDeInteresse = "<IMG SRC=\""
+){
+    $bCautela =
+        is_string($pHtmlParaSerAnalisado)
+        &&
+        strlen($pHtmlParaSerAnalisado)>0;
 
-$url = apodAbsoluteUrl(
-    18,
-    11,
-    21
-);
-function nomeParaDownload($pUrl){
-   // echo $pUrl;
-    $posImagem = strripos($pUrl,"/");
-    $nomeImagem = substr($pUrl,$posImagem+1);
-    echo $nomeImagem;
-    return $nomeImagem;
+    if ($bCautela){
+        $iPosOndeComecaMarcaDeInteresse =
+            //strpos, stripos retornam a primeira pos de match
+            //strrpos, strripos retornam a última pos de match
+            //ex strRightmostpos("ABBCCC", "B") -> 2
+            //ex strpos ("ABBCCC", "B") -> 1
+            stripos(
+                $pHtmlParaSerAnalisado,
+                $pStrMarcaDeInteresse
+            );
 
-}
-$html = getBinaryAtUrl($url);
-$url = urlDiretoParaImagemPorAnaliseDoHtml($html);
-//echo $url;
-//nomeParaDownload($url);
+        $bHaImagemDeInteresse =
+            $iPosOndeComecaMarcaDeInteresse!==false;
 
-//echo $html;
+        if ($bHaImagemDeInteresse){
+            $strPorcaoDoHtmlQueMereceNossaAtencao =
+                substr(
+                    $pHtmlParaSerAnalisado,
+                    $iPosOndeComecaMarcaDeInteresse
+                    + strlen($pStrMarcaDeInteresse)
+                );
+            $iPosDaAspaQueEncerraImagem =
+                stripos(
+                    $strPorcaoDoHtmlQueMereceNossaAtencao,
+                    "\"" //ou '"'
+                );
+            $urlRelativo = substr(
+                $strPorcaoDoHtmlQueMereceNossaAtencao,
+                0,
+                $iPosDaAspaQueEncerraImagem
+            );
 
-
-
-function hoeadAllMonthImages($pYear, $pMonth){
-    $urlsHtml = todosEnderecosDoMes($pYear,$pMonth);
-
-    foreach ($urlsHtml as $urlHtml){
-        $html = getBinaryAtUrl($urlHtml);
-        $urlRelativoImg = urlDiretoParaImagemPorAnaliseDoHtml($html);
-        $urlAbsImg = APOD_BASE_URL.$urlRelativoImg;
-        $nomeParaDl = nomeParaDownload($urlRelativoImg);
-        $byteDaImagem = getBinaryAtUrl($urlAbsImg);
-        file_put_contents($byteDaImagem,$nomeParaDl);
-
-    }
-}
-
-$urls201811 = todosEnderecosDoMes(18, 11);
-//var_dump($urls201811);
-
-function downloadDeImagemUrl($pUrl){
-
-    $html = getBinaryAtUrl($pUrl);
-    $url = urlDiretoParaImagemPorAnaliseDoHtml($html);
-    $binarioImagem = getBinaryAtUrl($url);
-    file_put_contents("BIN.JPG",$binarioImagem);
-}//downloadDeImagemUrl
-
-function urlDiretoParaImagemPorAnaliseDoHtml($pHtmlParaSerAnalizado){
-    $pStrMarcaDeInterece = "<IMG SRC=\"";
-    $bCautela = is_string($pHtmlParaSerAnalizado) && strlen($pHtmlParaSerAnalizado) > 0;
-    if($bCautela){
-
-        // strpos retorna a primeira posição de match
-        // strrpos retorna a última posição de match
-        $iPosOndeComecaMarcaDeInteresse = stripos($pHtmlParaSerAnalizado, $pStrMarcaDeInterece);
-        $bHaImagemDeInteresse = $iPosOndeComecaMarcaDeInteresse !== false;
-        if($bHaImagemDeInteresse){
-            $strPorcaoDoHtmlQueMereceNOssaAtencao = substr($pHtmlParaSerAnalizado, $iPosOndeComecaMarcaDeInteresse + strlen($pStrMarcaDeInterece));
-            $iPosDaAspaQueencerraImagem = stripos($strPorcaoDoHtmlQueMereceNOssaAtencao, "\"");
-            $urlRelativo = substr($strPorcaoDoHtmlQueMereceNOssaAtencao, 0 , $iPosDaAspaQueencerraImagem);
             return $urlRelativo;
         }
     }
-    return false;//Não foi possível extrair o url da imagem
+    return false; //não foi possível extrair o url relativo da imagem
 }//urlDiretoParaImagemPorAnaliseDoHtml
+
+//data hoarding
+
+function donwloadDeImagemNoUrl(
+    $pUrl
+){
+    /*
+     * na var $html ficará o source code da página APOD
+     * mencionada em $pUrl
+     * por exemplo, se $pUrl for
+     * https://apod.nasa.gov/apod/ap181102.html
+     * fica o HTML dessa página
+     *
+     * porque é que queremos ter acesso ao HTML
+     * para nele procurarmos a referência à imagem
+     * por análise de source codes em APOD já percebemos
+     * que, a haver uma imagem, será o ÚNICO elemento
+     * <IMG
+     */
+    $html = getBinaryAtUrl($pUrl);
+    $url =
+        urlDiretoParaImagemPorAnaliseDoHtml($html);
+    $binarioZerosUnsDaImagem = getBinaryAtUrl($url);
+    file_put_contents(
+        "BIN.JPG", //conseguir nomes corretos para files
+        $binarioZerosUnsDaImagem
+    );
+}//donwloadDeImagemNoUrl
+
+function hoardAllMonthImages(
+    $pYear,
+    $pMonth
+){
+    $urlsHtml = todosEnderecosDoMes($pYear, $pMonth);
+    foreach($urlsHtml as $urlHtml){
+        $html = getBinaryAtUrl($urlHtml);
+        $urlRelativoImg =
+            urlDiretoParaImagemPorAnaliseDoHtml($html);
+
+        if ($urlRelativoImg!==false){
+            $urlAbsImg = APOD_BASE_URL.$urlRelativoImg;
+            echo "About to dl: $urlAbsImg".PHP_EOL;
+
+            $nomeParaDl = nomeParaDownload($urlRelativoImg);
+            $bytesDaImagem = getBinaryAtUrl($urlAbsImg);
+
+            $bytesEscritos =
+                file_put_contents(
+                    $nomeParaDl,
+                    $bytesDaImagem
+                );
+            echo
+                "$bytesEscritos byte(s) escritos para $nomeParaDl.".
+                PHP_EOL;
+        }
+
+    }//foreach
+}//hoardAllMonthImages
+
+/*
+ * escreva uma solução
+ * "nomeParaDownload", que recebido um URL relativo
+ * como o exemplificado de seguida
+ * image/1811/JupiterSwirls_JunoBrealey_960.jpg
+ * retorna um nome adequado para download do conteúdo
+ * referido, neste caso
+ * JupiterSwirls_JunoBrealey_960.jpg
+ */
+function nomeParaDownload (
+    $pUrlRelativo
+){
+    $ret = "";
+
+    $iPosDePartida = strrpos($pUrlRelativo, "/")+1;
+    $ret= substr(
+        $pUrlRelativo,
+        $iPosDePartida
+    );
+
+    return $ret;
+}//nomeParaDownload
+
+/*
+echo nomeParaDownload
+("image/1811/JupiterSwirls_JunoBrealey_960.jpg");
+*/
+
+hoardAllMonthImages(18, 10); 
